8510ae3ace7465f86dbd2417b4b7e942904ad8f5
gamoradima <gamoradima@gmail.com>
2024-12-19 14:03:44 +0200

    Realty max price web service example

---
+++ UsrRealty/Autogenerated/Src/UsrRealtyServiceSchema.UsrRealty.cs
@@ -0,0 +1,57 @@
+﻿namespace Terrasoft.Configuration
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Globalization;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+
+	#region Class: UsrRealtyServiceSchema
+
+	/// <exclude/>
+	public class UsrRealtyServiceSchema : Terrasoft.Core.SourceCodeSchema
+	{
+
+		#region Constructors: Public
+
+		public UsrRealtyServiceSchema(SourceCodeSchemaManager sourceCodeSchemaManager)
+			: base(sourceCodeSchemaManager) {
+		}
+
+		public UsrRealtyServiceSchema(UsrRealtyServiceSchema source)
+			: base( source) {
+		}
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected override void InitializeProperties() {
+			base.InitializeProperties();
+			UId = new Guid("7039450c-9de4-4065-aa4b-0891195ecc75");
+			Name = "UsrRealtyService";
+			ParentSchemaUId = new Guid("50e3acc0-26fc-4237-a095-849a1d534bd3");
+			CreatedInPackageId = new Guid("837b972d-db96-4eaf-8d7e-147049d0fd64");
+			ZipBody = new byte[] { 31,139,8,0,0,0,0,0,4,0,43,206,207,77,45,201,200,204,75,87,200,204,83,112,86,6,0,83,0,231,29,15,0,0,0 };
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		public override void GetParentRealUIds(Collection<Guid> realUIds) {
+			base.GetParentRealUIds(realUIds);
+			realUIds.Add(new Guid("7039450c-9de4-4065-aa4b-0891195ecc75"));
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
